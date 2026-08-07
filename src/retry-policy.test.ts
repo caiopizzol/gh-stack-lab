067c6d3fb2ed1529cae725400567660a28860474
Caio Pizzol <caiopizzol@icloud.com>
2026-08-07 16:46:30 -0300

    feat(retry): add default retry policy

---
+++ src/retry-policy.test.ts
@@ -0,0 +1,11 @@
+import { expect, test } from "bun:test";
+
+import { DEFAULT_RETRY_POLICY } from "./retry-policy";
+
+test("uses exponential growth by default", () => {
+  expect(DEFAULT_RETRY_POLICY).toEqual({
+    baseDelayMs: 100,
+    maxDelayMs: 5_000,
+    multiplier: 2,
+  });
+});
